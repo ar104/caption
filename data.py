2b324084f069037079ab3d1998f5db03da8ef89f
Ubuntu <aroy@dsvm.n5dngojki5aezaas5a3p30oqqe.bx.internal.cloudapp.net>
2020-10-11 23:47:43 +0000

    Vocabulary pre-processing tools.

---
+++ data.py
@@ -0,0 +1,85 @@
+def build_annotations_vocab(fname):
+    """ 
+    Build an annotations vocabulary for the flickr8k dataset.
+    Take a string filename as input.
+    Returns a dictionary mapping words to token numbers
+    and a dictionary mapping images to a list of tokens.
+    """
+    image_to_tokens = {}
+    vocab = {'<START>' : 0}
+    with open(fname) as f:
+        lines = f.readlines()
+        for line in lines:
+            key, desc = line.split('\t')
+            try:
+                jpeg, ann_num = key.split('#')
+            except Exception as e:
+                print('failed token key parsing on {}'.format(key))
+                raise e
+            if ann_num != '0':
+                continue
+            words = desc.strip('\n').split(' ')
+            tokens = []
+            for word in words:
+                if len(word) < 2:
+                    continue
+                if word not in vocab:
+                    vocab[word] = len(vocab)
+                tokens.append(vocab[word])
+            image_to_tokens[jpeg] = [vocab['<START>']] + tokens
+    vocab['<STOP>'] = len(vocab)
+    return vocab, image_to_tokens
+
+def load_annotations_vocab(fname):
+    """
+    Load annotations vocabulary from a file.
+    Takes filename as string argument.
+    Returns a list of words corresponding to integer
+    token indices.
+    """
+    token_to_word = {}
+    with open(fname) as f:
+        lines = f.readlines()
+        for line in lines:
+            w, index = line.strip('\n').split(',')
+            token_to_word[index] = w
+    token_to_word_list = [0] * len(token_to_word)
+    for k,v in token_to_word.items():
+        token_to_word_list[int(k)] = v
+    return token_to_word_list
+
+def load_annotations_tokens(fname, stop_token):
+    """
+    Load annotations token from a file.
+    Takes filename as string argument and a stop token number
+    Returns a list of dictionary of images to tokens with
+    an added stop token at the end.
+    """
+    image_to_tokens = {}
+    with open(fname) as f:
+        lines = f.readlines()
+        for line in lines:
+            items = line.strip('\n').split(',')
+            image_to_tokens[items[0]] = [int(k) for k in items[1:]]
+            image_to_tokens[items[0]].append(stop_token)
+    return image_to_tokens
+
+# Quick test
+def quick_test():
+    vocab, image_to_tokens = build_annotations_vocab('/home/aroy/notebooks/experiments/Flickr8k_text/Flickr8k.token.txt')
+    with open('/home/aroy/notebooks/experiments/Flickr8k_text/Flickr8k.vocab.txt', 'w') as f:
+        for w, index in vocab.items():
+            f.write('{},{}\n'.format(w, index))
+    with open('/home/aroy/notebooks/experiments/Flickr8k_text/Flickr8k.image_to_tokens.txt', 'w') as f:
+        for w, tokens in image_to_tokens.items():
+            f.write('{}'.format(w))
+            for t in tokens:
+                f.write(',{}'.format(t))
+            f.write('\n')
+    token_to_word = load_annotations_vocab('/home/aroy/notebooks/experiments/Flickr8k_text/Flickr8k.vocab.txt')
+    image_to_tokens = load_annotations_tokens('/home/aroy/notebooks/experiments/Flickr8k_text/Flickr8k.image_to_tokens.txt', len(token_to_word) - 1)
+    for t in image_to_tokens['1001773457_577c3a7d70.jpg']:
+        print(token_to_word[t])
+
+if __name__ == '__main__':
+    quick_test()
