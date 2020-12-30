93913ad72f7858f84f53f4e8a16e16142d85b67b
Ubuntu <aroy@dsvm.fvf1ac3cza5udjbqursa3rk5ya.bx.internal.cloudapp.net>
2020-12-30 06:21:19 +0000

    Add dropout.

--- model.py
+++ model.py
@@ -2,13 +2,15 @@ import tensorflow as tf
 import numpy as np
 import data
 
-def make_model(lstm_units, embedding_size, max_caption_length, vocab_size):
+def make_model(lstm_units, embedding_size, max_caption_length, vocab_size, dropout_rate=0.0):
     vgg_model = tf.keras.applications.vgg16.VGG16()
     base_model = tf.keras.Model(inputs=vgg_model.input, outputs=tf.keras.layers.Reshape(target_shape=(196, 512))(vgg_model.get_layer('block5_conv3').output))
     base_model.trainable = False
     conv_features = base_model(vgg_model.input)
     h_init = tf.keras.layers.Dense(units=lstm_units, activation = 'relu')(tf.keras.layers.Flatten()(conv_features))
+    h_init = tf.keras.layers.Dropout(rate=dropout_rate)(h_init)
     c_init = tf.keras.layers.Dense(units=lstm_units, activation = 'relu')(tf.keras.layers.Flatten()(conv_features))
+    c_init = tf.keras.layers.Dropout(rate=dropout_rate)(c_init)
     lstm = tf.keras.layers.LSTM(units=lstm_units, return_state=True)
     attention = tf.keras.layers.Attention()
     token_inputs = [tf.keras.Input(shape=(1,)) for _ in range(max_caption_length)]
@@ -18,12 +20,13 @@ def make_model(lstm_units, embedding_size, max_caption_length, vocab_size):
     output_symbols = []
     for i in range(max_caption_length - 1):
         attention_query = attention_projection(h_init)
+        attention_query = tf.keras.layers.Dropout(rate=dropout_rate)(attention_query)
         attention_input = attention([tf.keras.layers.Reshape(target_shape=(1, 512))(attention_query), conv_features])
         token_input = input_embedding(token_inputs[i])
         token_input  = tf.keras.layers.Reshape(target_shape=(1, embedding_size))(token_input)
         lstm_input = tf.keras.layers.concatenate([attention_input, token_input])
         output, h_init, c_init = lstm(inputs=lstm_input, initial_state=[h_init, c_init])
-        output_symbols.append(tf.keras.layers.Reshape(target_shape=(1, vocab_size))(output_net(output)))
+        output_symbols.append(tf.keras.layers.Reshape(target_shape=(1, vocab_size))(tf.keras.layers.Dropout(rate=dropout_rate)(output_net(output))))
     output_array = tf.keras.layers.concatenate(output_symbols, axis = -2)
     final_model = tf.keras.Model(inputs=[base_model.input] + token_inputs, outputs=output_array)
     final_model.compile(optimizer='Adam', loss='sparse_categorical_crossentropy')
@@ -61,7 +64,7 @@ def quick_test():
     test_input = [img_array] + token_array
     test_output_list = token_array[1:]
     test_output = np.concatenate(test_output_list, axis = -1)
-    model.fit(test_input, test_output, batch_size=2, epochs=10)
+    model.fit(test_input, test_output, batch_size=2, epochs=500)
     predict_output = model.predict(test_input, batch_size=2)
     display_results(img_array, token_array, predict_output, 0)
 
