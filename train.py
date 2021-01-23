761a4b89f44169f3f631d39fb64691fbf95b0f87
Amitabha Roy <amitabha.roy@gmail.com>
2021-01-23 18:08:23 -0500

    Store variable length caption array.

--- train.py
+++ train.py
@@ -34,23 +34,25 @@ def preprocess(train_fname, test_fname):
         img_path = bytes.decode(fname.numpy())
         img_array = tf.convert_to_tensor(data.load_image(img_path, include_batch=True))
         img_name = img_path.split('/')[-1]
+        ret = [tf.convert_to_tensor(np.asarray([len(image_to_tokens[img_name])]))]
         token_list = data.pad(image_to_tokens[img_name], max_caption_length, stop_symbol)
-        ret = [img_array]
+        ret.append(img_array)
         for t in token_list:
             ret.append(tf.convert_to_tensor(np.asarray([t])))
         return ret
     def save_items(ds, fp_image, fp_caption):
         saved = 0
         for one in ds:
-            conv_features = base_model.predict(one[0], batch_size=1)
+            caption_len = one[0]
+            conv_features = base_model.predict(one[1], batch_size=1)
             conv_features = np.reshape(conv_features, newshape = (196, 512))
             np.save(fp_image, conv_features, allow_pickle=True)
-            np.save(fp_caption, one[1], allow_pickle=True)
+            np.save(fp_caption, one[2][:caption_len], allow_pickle=True)
             saved += 1
             print("Saved = {}".format(saved))
     
-    dataset = dataset.map(lambda x: tf.py_function(load_image, [x], [tf.float32] + [tf.int64]*max_caption_length))
-    dataset = dataset.map(lambda *x: (x[0], tf.concat(x[1:], axis=-1)))
+    dataset = dataset.map(lambda x: tf.py_function(load_image, [x], [tf.int64, tf.float32] + [tf.int64]*max_caption_length))
+    dataset = dataset.map(lambda *x: (x[0], x[1], tf.concat(x[2:], axis=-1)))
     val_dataset = dataset.take(192)
     train_dataset = dataset.skip(192)
     train_image_fp = open(train_fname + '_image', "wb")
@@ -60,6 +62,32 @@ def preprocess(train_fname, test_fname):
     save_items(val_dataset, test_image_fp, test_caption_fp)
     save_items(train_dataset, train_image_fp, train_caption_fp)
     # print(dataset.cardinality().numpy())
+
+
+def ds_gen(file_image, file_caption):
+    data_images = []
+    data_captions = []
+    def preload(max_count=64):
+        fp_image = open(file_image, 'rb')
+        fp_caption = open(file_caption, 'rb')
+        count = 0
+        while count != max_count:
+            try:
+                data_images.append(np.load(fp_image, allow_pickle=True))
+                captions = np.load(fp_caption, allow_pickle=True)
+                padded_captions = np.pad(captions, max_caption_length, constant_values=stop_symbol)
+                data_captions.append(padded_captions)
+            except (OSError, IOError):
+                break
+            count += 1
+    def generator():
+        for (im, cap) in zip(data_images, data_captions):
+            yield (im, cap)
+        return
+    print("preloading ...")
+    preload(max_count=-1)
+    print("done!")
+    return lambda: generator()
     
 def train():
     print(device_lib.list_local_devices())
@@ -68,30 +96,7 @@ def train():
     stop_symbol=vocab_size - 1
     image_to_tokens = data.load_annotations_tokens('/datadrive/flickr8k/Flickr8k.image_to_tokens.txt', stop_symbol)
     max_caption_length=max([len(t) for t in image_to_tokens.values()])
-    caption_model = model.make_model(1024, 100, max_caption_length, vocab_size, dropout_rate=0.3)
-    def ds_gen(file_image, file_caption):
-        data_images = []
-        data_captions = []
-        def preload(max_count=64):
-            fp_image = open(file_image, 'rb')
-            fp_caption = open(file_caption, 'rb')
-            count = 0
-            while count != max_count:
-                try:
-                    data_images.append(np.load(fp_image, allow_pickle=True))
-                    data_captions.append(np.load(fp_caption, allow_pickle=True))
-                except (OSError, IOError):
-                    break
-                count += 1
-        def generator():
-            for (im, cap) in zip(data_images, data_captions):
-                yield (im, cap)
-            return
-        print("preloading ...")
-        preload(max_count=-1)
-        print("done!")
-        return lambda: generator()
-        
+    caption_model = model.make_model(1024, 100, max_caption_length, vocab_size, dropout_rate=0.3)        
     train_dataset = tf.data.Dataset.from_generator(ds_gen('blah_train_image', 'blah_train_caption'),
                                                    output_types=(tf.float32, tf.int64), output_shapes=((196, 512), (max_caption_length,)))
                                                    #output_signature = (tf.TensorSpec(shape=(1, None), dtype=tf.float32), tf.TensorSpec(shape=(max_caption_length,))))
@@ -121,18 +126,6 @@ def check_perf():
     image_to_tokens = data.load_annotations_tokens('/datadrive/flickr8k/Flickr8k.image_to_tokens.txt', stop_symbol)
     max_caption_length=max([len(t) for t in image_to_tokens.values()])
     caption_model = model.make_model(1024, 100, max_caption_length, vocab_size, dropout_rate=0.3)
-    def ds_gen(file_image, file_caption):
-        def generator():
-            fp_image = open(file_image, 'rb')
-            fp_caption = open(file_caption, 'rb')
-            while True:
-                try:
-                    data_image = np.load(fp_image, allow_pickle=True)
-                    data_caption = np.load(fp_caption, allow_pickle=True)
-                except (OSError, IOError):
-                    return
-                yield (data_image, data_caption)
-        return generator()
     caption_model.load_weights('caption_model.269-1.12.h5')
     val_dataset = tf.data.Dataset.from_generator(lambda: ds_gen('blah_test_image', 'blah_test_caption'),
                                                    output_types=(tf.float32, tf.int64), output_shapes=((196, 512), (max_caption_length,)))
